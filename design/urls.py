d2c217e334964bbef684d0e2ab07efd09b46e954
caferAsi <caf.as.20020706@gmail.com>
2023-09-16 16:20:15 +0300

    SET get_POST Metod

--- design/urls.py
+++ design/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('ww/', WW, name='WW'),
+    path('getValue/',getValue, name='getValue'),
 ]
--- design/views.py
+++ design/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,5 +6,16 @@ from django.shortcuts import render
 def index(request):
     return render(request,'design/index.html')
 
+def getValue(request):
+    print(request.method)
+    if request.method=='GET':
+        print(request.GET)
+        return  HttpResponse ('{"get":"get"}')
+    if request.method=='POST':
+        return  HttpResponse('{"POST":"POST"}')    
+    return HttpResponse('scdsafce')
+
+
 def WW(request):
+   
     return render(request,'design/warp-weft-def.html')
